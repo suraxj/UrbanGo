51f0996057abe63dc6e3ac5e39d9cbde9fdbc2d7
suraxj <prakashsinghsuraj69@gmail.com>
2025-11-13 15:19:44 +0530

    User Registration Done

--- backend/app.js
+++ backend/app.js
@@ -3,19 +3,19 @@ dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
-const captainRoutes = require('./routes/captain.routes');
-const mapsRoutes = require('./routes/maps.routes');
-const rideRoutes = require('./routes/ride.routes');
+// const captainRoutes = require('./routes/captain.routes');
+// const mapsRoutes = require('./routes/maps.routes');
+// const rideRoutes = require('./routes/ride.routes');
 
 connectToDb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+// app.use(cookieParser());
 
 
 
@@ -24,9 +24,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/users', userRoutes);
-app.use('/captains', captainRoutes);
-app.use('/maps', mapsRoutes);
-app.use('/rides', rideRoutes);
+// app.use('/captains', captainRoutes);
+// app.use('/maps', mapsRoutes);
+// app.use('/rides', rideRoutes);
 
 
 
